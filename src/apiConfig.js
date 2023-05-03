c89cd44f263560e16968e745e361c0a59d69ce14
YbotMan <toby.balsley+YbotMan@gmail.com>
2023-05-03 17:41:42 -0400

    some significant api changes

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,1 +1,5 @@
-export const apiUrl = "http://localhost:5001";
+export const jsonServerUrl = "http://localhost:5001";
+export const nextApiUrl =
+    process.env.NODE_ENV === "production"
+        ? "https://yourdomain.com"
+        : "http://localhost:3000";
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,14 @@ import axios from "axios";
 import Modal from "react-modal";
 import Image from "next/image";
 import { apiUrl } from "../apiConfig";
+import { jsonServerUrl, nextApiUrl } from "../apiConfig";
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import Grid from "@mui/material/Grid";
 
+//const { data } = await axios.get(`${jsonServerUrl}/images`);
+//const { data: savedImage } = await axios.put(`${jsonServerUrl}/images`, updatedImage);
+//const { data } = await axios.post(`${nextApiUrl}/api/upload`, formData, config);
 
 Modal.setAppElement("#__next");
 
@@ -48,29 +52,35 @@ const Index = () => {
 
   const fetchImages = async () => {
     try {
-      const { data } = await axios.get(`${apiUrl}/images`);
+      const { data } = await axios.get(`${jsonServerUrl}/images`);
+      //const { data } = await axios.get(`${apiUrl}/images`);
       setImages(data);
     } catch (error) {
       console.error("Error fetching images:", error);
     }
   };
-  // const fetchImages = async () => {
-  //   const { data } = await axios.get("api/images");
-  //   setImages(data);
-  // };
 
-  const handleImageUpload = async (files) => {
-    const formData = new FormData();
-    formData.append("file", files[0]);
-    const { data } = await axios.post("/upload", formData, { headers: { "Content-Type": "multipart/form-data" } });
-    const newImage = { ...data, attributes: {} };
-    const { data: savedImage } = await axios.post("api/images", newImage);
-    setImages([...images, savedImage]);
+  const handleImageUpload = async (file) => {
+    try {
+      const formData = new FormData();
+      formData.append("file", file);
+      //const { data } = await axios.post(`${nextApiUrl}/api/upload`, formData, config);
+      const { data } = await axios.post(`${nextApiUrl}/api/upload`, formData, {
+        //const { data } = await axios.post(`${apiUrl}/api/upload`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      setImageUrl(data.filePath);
+    } catch (error) {
+      console.error("Error uploading image:", error);
+    }
   };
 
   const handleUpdateAttributes = async (updatedAttributes) => {
     const updatedImage = { ...selectedImage, attributes: updatedAttributes };
-    const { data: savedImage } = await axios.put("api/images", updatedImage);
+    const { data: savedImage } = await axios.put(`${jsonServerUrl}/images`, updatedImage);
+    //const { data: savedImage } = await axios.put("api/images", updatedImage);
     setImages(images.map((img) => (img.id === savedImage.id ? savedImage : img)));
     setSelectedImage(null);
   };
